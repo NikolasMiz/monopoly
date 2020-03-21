252cfe8e24a4d63ac0530d69ca8037a5fdd2dffe
Nicholas <miazzo.nicholas98@gmail.com>
2020-03-21 14:55:22 +0100

    Update settings.py

--- webapps/settings.py
+++ webapps/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  'ec2-18-188-96-54.us-east-2.compute.amazonaws.com',
-                 '18.188.96.54']
+                 '18.188.96.54',
+                  '0.0.0.0' ]
 
 # Application definition
 
